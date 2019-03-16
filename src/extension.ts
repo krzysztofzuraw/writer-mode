e73534c908c6ce48fe32086757764eed6bd2b7e5
Krzysztof Żuraw <krzysztofzuraw@fastmail.com>
2019-03-16 18:46:16 +0100

    feat: basic writer mode implementation

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,58 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const RULERS_SECTION = "editor.rulers";
+const FONTSIZE_SECTION = "editor.fontSize";
+
 export function activate(context: vscode.ExtensionContext) {
+  const activateCommand = vscode.commands.registerCommand(
+    "extension.writerModeOn",
+    () => {
+      // Display a message box to the user
+      vscode.window.showInformationMessage("Writer mode is on.");
+      // Activate zen mode
+      vscode.commands.executeCommand("workbench.action.toggleZenMode");
+      overrideSettings([], 20);
+    }
+  );
+  const deactivateCommand = vscode.commands.registerCommand(
+    "extension.writerModeOff",
+    () => {
+      // Display a message box to the user
+      vscode.window.showInformationMessage("Writer mode is off.");
+      vscode.commands.executeCommand("workbench.action.exitZenMode");
+      overrideSettings(undefined, undefined);
+    }
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "writer-mode" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(activateCommand, deactivateCommand);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+function overrideSettings(rulersValue?: number[], fontSizeValue?: number) {
+  const config = vscode.workspace.getConfiguration();
+
+  if (vscode.workspace.workspaceFolders) {
+    // Override default workspace settings
+    config.update(
+      RULERS_SECTION,
+      rulersValue,
+      vscode.ConfigurationTarget.Workspace
+    );
+    config.update(
+      FONTSIZE_SECTION,
+      fontSizeValue,
+      vscode.ConfigurationTarget.Workspace
+    );
+  } else {
+    // Override global settings
+    config.update(
+      RULERS_SECTION,
+      rulersValue,
+      vscode.ConfigurationTarget.Global
+    );
+    config.update(
+      FONTSIZE_SECTION,
+      fontSizeValue,
+      vscode.ConfigurationTarget.Global
+    );
+  }
+}
